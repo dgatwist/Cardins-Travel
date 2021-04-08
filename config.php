2e14448b874d0a5eab9d8ac03f2d8d4096bd5679
Andy Nyenimigabo <51054119+dgatwist@users.noreply.github.com>
2021-04-08 17:35:08 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname="travel_db";
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname="travel_db";
+
+$servername = "remotemysql.com";
+$username = "cLb5xOFkmA";
+$password = "B0QlCVvG8s";
+$dbname="cLb5xOFkmA";
 
 
 $conn = mysqli_connect($servername, $username, $password,$dbname);
